f7b872bef8f58482dc838368fd52037a15216bef
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-06-12 19:00:09 +0000

    Create tours.ts

---
+++ src/data/tours.ts
@@ -0,0 +1,34 @@
+import { Tour } from "@/types/tour";
+
+export const tours: Tour[] = [
+  {
+    id: 1,
+    title: "Bali Adventure Escape",
+    image:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4",
+    location: "Indonesia",
+    duration: "7 Days",
+    price: 1200,
+    rating: 4.9
+  },
+  {
+    id: 2,
+    title: "Luxury Dubai Experience",
+    image:
+      "https://images.unsplash.com/photo-1512453979798-5ea266f8880c",
+    location: "UAE",
+    duration: "5 Days",
+    price: 1800,
+    rating: 5.0
+  },
+  {
+    id: 3,
+    title: "Paris Romantic Tour",
+    image:
+      "https://images.unsplash.com/photo-1431274172761-fca41d930114",
+    location: "France",
+    duration: "6 Days",
+    price: 1600,
+    rating: 4.8
+  }
+];
