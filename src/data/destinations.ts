c9c3cdf80949b7b5cbe17e799e3d3c0b6a86edc9
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-06-12 18:59:17 +0000

    Create destinations.ts

---
+++ src/data/destinations.ts
@@ -0,0 +1,32 @@
+import { Destination } from "@/types/destination";
+
+export const destinations: Destination[] = [
+  {
+    id: 1,
+    name: "Bali",
+    image:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4",
+    tours: 25
+  },
+  {
+    id: 2,
+    name: "Dubai",
+    image:
+      "https://images.unsplash.com/photo-1512453979798-5ea266f8880c",
+    tours: 15
+  },
+  {
+    id: 3,
+    name: "Paris",
+    image:
+      "https://images.unsplash.com/photo-1431274172761-fca41d930114",
+    tours: 30
+  },
+  {
+    id: 4,
+    name: "Cape Town",
+    image:
+      "https://images.unsplash.com/photo-1576485290814-1c72aa4bbb8e",
+    tours: 20
+  }
+];
